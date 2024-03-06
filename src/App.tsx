9fa40b00dc34e675598cd3132addd0a8c783de59
Emanuele Brotti <tipocippo@gmail.com>
2024-03-06 09:08:33 +0100

    added portfolio section

--- src/App.tsx
+++ src/App.tsx
@@ -27,13 +27,17 @@ function App() {
         </h2>
       </div>
       
-      <div className="w-100 h-300 flex flex-col p-4 bg-light text-dark">
+      <div className="w-100 h-fit flex flex-col p-4 bg-light text-dark mb-5">
         <h2 className="text-2xl xsm:text-4xl sm:text-5xl md:text-6xl text-wrap xsm:text-nowrap title font-bold mt-5">
             DON'T WASTE YOUR TIME, <br></br>
-            HERE'S MY PORTFOLIO.
+            HERE'S MY PORTFOLIO. 
         </h2>
       </div>
       
+      <div className="w-100 h-fit bg-dark text-light p-4 grid grid-flow-row grid-cols-1 sm:grid-cols-2 gap-4">
+        {/*portfolio elements*/}
+
+      </div>
       
     </>
   );
